0eabbd47dd4fa252a743cc3b51cced969b92c05e
Adachi Hideo <higan.ibn5100@gmail.com>
2020-07-19 23:34:06 +0900

    add

---
+++ nextWeekend/bvh.h
@@ -0,0 +1,37 @@
+class bvh_node : public hittable {
+    public:
+        bvh_node();
+
+        bvh_node(hittable_list& list, double time0, double time1)
+            : bvh_node(list.objects, 0, list.objects.size(), time0, time1)
+        {}
+
+        bvh_node(
+            std::vector<shared_ptr<hittable>>& objects,
+            size_t start, size_t end, double time0, double time1);
+
+        virtual bool hit(const ray& r, double tmin, double tmax, hit_record& rec) const;
+        virtual bool bounding_box(double t0, double t1, aabb& output_box) const;
+
+    public:
+        shared_ptr<hittable> left;
+        shared_ptr<hittable> right;
+        aabb box;
+};
+
+bool bvh_node::bounding_box(double t0, double t1, aabb& output_box) const {
+    output_box = box;
+    return true;
+}
+
+
+bool bvh_node::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+    if (!box.hit(r, t_min, t_max))
+        return false;
+    
+    bool hit_left = left->hit(r, t_min, t_max, rec);
+    // 
+    bool hit_right = right->hit(r, t_min, hit_left ? rec.t : t_max, rec);
+
+    return hit_left || hit_right;
+}
--- nextWeekend/hittable.h
+++ nextWeekend/hittable.h
@@ -12,6 +12,8 @@ struct hit_record {
     vec3 normal;
     shared_ptr<material> mat_ptr;
     double t;
+    double u;
+    double v;
     bool front_face;
     // 法線は物体の中に向かう向きと外に向かう向きの２パターンがある． 
     inline void set_face_normal(const ray& r, const vec3& outward_normal) {
